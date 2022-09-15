b06513727b416c272c45cddf8d314b3e53c5da07
Stefan Siegl <stesie@brokenpipe.de>
2022-09-16 00:22:44 +0200

    change title

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -48,7 +48,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'WueWW App',
+            title: 'Stadtfest App',
             template: './public/index.html',
         }),
         new MiniCssExtractPlugin({
